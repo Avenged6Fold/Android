fbcc2748de3671ac7376245331838437ed1a23e4
danuugithub <akbardanu631@gmail.com>
2024-11-20 07:13:48 +0700

    penambahan item fitur dan iconnya

---
+++ app/src/main/java/com/project/jemberliburan/Model/Fitur.java
@@ -0,0 +1,13 @@
+package com.project.jemberliburan.Model;
+
+public class Fitur {
+    private int imageResId;
+
+    public Fitur(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+}
