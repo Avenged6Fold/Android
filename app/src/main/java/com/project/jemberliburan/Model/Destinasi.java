0a3ddf609a9aefc82b56f674649f92091e7e3bac
danuugithub <akbardanu631@gmail.com>
2024-11-20 07:15:28 +0700

    penambahan destinasi

---
+++ app/src/main/java/com/project/jemberliburan/Model/Destinasi.java
@@ -0,0 +1,13 @@
+package com.project.jemberliburan.Model;
+
+public class Destinasi {
+    private int imageResId;
+
+    public Destinasi(int imageResId) {
+        this.imageResId = imageResId;
+    }
+
+    public int getImageResId() {
+        return imageResId;
+    }
+}
