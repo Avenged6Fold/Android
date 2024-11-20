8c37cb797ef553e92029d9be355e90508958c1f0
Jimmy-hubb <Shenriu44@gmail.com>
2024-11-20 17:09:19 +0700

    Update Db_Contract.java
    Menambahkan String reset-password.php, verify-code.php

--- app/src/main/java/com/project/jemberliburan/Connection/Db_Contract.java
+++ app/src/main/java/com/project/jemberliburan/Connection/Db_Contract.java
@@ -2,10 +2,12 @@ package com.project.jemberliburan.Connection;
 
 public class Db_Contract {
 
-    public static String ip = "192.168.1.24";
+    public static String ip = "10.0.2.2"; // IP untuk emulator (localhost)
 
-    public static final String  urlRegisterActivity  = "http://"+ip+"/Jeli_API//api-register.php";
-    public static final String urlLoginActivity = "http://"+ip+"/Jeli_API//api-login.php";
-
+    public static final String urlRegisterActivity = "http://" + ip + "/Jeli_API/api-register.php";
+    public static final String urlLoginActivity = "http://" + ip + "/Jeli_API/api-login.php";
+    public static final String urlSendVerificationCode = "http://" + ip + "/Jeli_API/send-verification-code.php";
+    public static final String urlResetPassword = "http://" + ip + "/Jeli_API/reset-password.php"; // Tambahkan URL reset password
+    public static final String urlVerifyCode = "http://" + ip + "/Jeli_API/verify-code.php";
 
 }
