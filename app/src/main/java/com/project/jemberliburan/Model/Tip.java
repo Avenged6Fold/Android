6c6de945585d908e3ce3f1c2bf3df81b36739eb5
danuugithub <akbardanu631@gmail.com>
2024-11-20 07:16:21 +0700

    mengubah activity dan layoutnya

--- app/src/main/java/com/project/jemberliburan/Model/Tip.java
+++ app/src/main/java/com/project/jemberliburan/Model/Tip.java
@@ -1,22 +1,10 @@
 package com.project.jemberliburan.Model;
 
 public class Tip {
-    private String title;
-    private String description;
     private int imageResId;
 
-    public Tip(String title, String description, int imageResId) {
-        this.title = title;
-        this.description = description;
+    public Tip(int imageResId) {
         this.imageResId = imageResId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
     }
 
     public int getImageResId() {
--- app/src/main/java/com/project/jemberliburan/adapter/DestinasiAdapter.java
+++ app/src/main/java/com/project/jemberliburan/adapter/DestinasiAdapter.java
@@ -1,11 +1,14 @@
 package com.project.jemberliburan.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import com.project.jemberliburan.R;
 import java.util.List;
@@ -13,36 +16,73 @@ import java.util.List;
 public class DestinasiAdapter extends RecyclerView.Adapter<DestinasiAdapter.ViewHolder> {
 
     private Context context;
-    private List<Integer> destinationImages;
+    private List<DestinasiItem> destinasiList;
 
-    public DestinasiAdapter(Context context, List<Integer> destinationImages) {
+    public DestinasiAdapter(Context context, List<DestinasiItem> destinasiList) {
         this.context = context;
-        this.destinationImages = destinationImages;
+        this.destinasiList = destinasiList;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.activity_destinasi_adapter, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_destinasi, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.imageView.setImageResource(destinationImages.get(position));
+        DestinasiItem item = destinasiList.get(position);
+        holder.imageView.setImageResource(item.getImageResId());
+        holder.nameTextView.setText(item.getName());
+
+        // Set klik listener untuk card
+        holder.cardView.setOnClickListener(v -> {
+            Intent intent = new Intent(context, item.getDestinationActivity());
+            context.startActivity(intent);
+        });
     }
 
     @Override
     public int getItemCount() {
-        return destinationImages.size();
+        return destinasiList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        TextView nameTextView;
+        CardView cardView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.destinationImage);
+            nameTextView = itemView.findViewById(R.id.destinationName);
+            cardView = itemView.findViewById(R.id.cardView);
+        }
+    }
+
+    // Model untuk setiap item
+    public static class DestinasiItem {
+        private String name;
+        private int imageResId;
+        private Class<?> destinationActivity;
+
+        public DestinasiItem(String name, int imageResId, Class<?> destinationActivity) {
+            this.name = name;
+            this.imageResId = imageResId;
+            this.destinationActivity = destinationActivity;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getImageResId() {
+            return imageResId;
+        }
+
+        public Class<?> getDestinationActivity() {
+            return destinationActivity;
         }
     }
 }
