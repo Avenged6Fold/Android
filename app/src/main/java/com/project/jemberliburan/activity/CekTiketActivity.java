90c97f201f83ac559ae5f8667eb9d92dc3ef4735
danuugithub <akbardanu631@gmail.com>
2024-12-02 20:09:07 +0700

    penambahan activity dan layout

---
+++ app/src/main/java/com/project/jemberliburan/activity/CekTiketActivity.java
@@ -0,0 +1,28 @@
+package com.project.jemberliburan.activity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.project.jemberliburan.R;
+
+public class CekTiketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cek_tiket);
+
+        ImageView backButton = findViewById(R.id.icon_back);
+
+        // Tambahkan listener untuk icon_back
+        backButton.setOnClickListener(v -> finish());
+
+    }
+}
---
+++ app/src/main/java/com/project/jemberliburan/activity/RiwayatTiketActivity.java
@@ -0,0 +1,28 @@
+package com.project.jemberliburan.activity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.project.jemberliburan.R;
+
+public class RiwayatTiketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_riwayat_tiket);
+
+        ImageView backButton = findViewById(R.id.icon_back);
+
+        // Tambahkan listener untuk icon_back
+        backButton.setOnClickListener(v -> finish());
+
+    }
+}
